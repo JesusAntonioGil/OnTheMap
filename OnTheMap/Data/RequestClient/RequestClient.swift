3ceee7dd8bb94bea98e8a67966e7e3bac7f576b1
Jesus <jesus.a22@hotmail.com>
2016-06-25 17:20:56 +0200

    Put location on map

--- OnTheMap/Data/RequestClient/RequestClient.swift
+++ OnTheMap/Data/RequestClient/RequestClient.swift
@@ -86,6 +86,7 @@ class RequestClient: NSObject {
         request.addValue("QrX47CA9cyuGewLdsL7o5Eb8iug6Em8ye0dnAbIr", forHTTPHeaderField: "X-Parse-Application-Id")
         request.addValue("QuWThTdiRmTux3YaDseUSEpUKo7aBYM737yKd4gY", forHTTPHeaderField: "X-Parse-REST-API-Key")
         request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.HTTPMethod = target.method
         
         if(target.method == "PUT") {
             request.HTTPBody = httpBody(target.body)
